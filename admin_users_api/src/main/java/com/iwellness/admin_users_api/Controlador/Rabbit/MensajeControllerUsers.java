801cda85b648afb4ca5025c9212444de46b34c5a
Sara Peña <sarapenagomez02@gmail.com>
2025-04-13 18:40:27 -0500

    update message publish
    Modifique como se envia el mensaje desde Usuarios para que envie solo lo necesario

--- admin_users_api/src/main/java/com/iwellness/admin_users_api/Controlador/Rabbit/MensajeControllerUsers.java
+++ admin_users_api/src/main/java/com/iwellness/admin_users_api/Controlador/Rabbit/MensajeControllerUsers.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iwellness.admin_users_api.DTO.UsuariosDTO;
 import com.iwellness.admin_users_api.Entidades.Proveedor;
 import com.iwellness.admin_users_api.Entidades.Usuarios;
 import com.iwellness.admin_users_api.Servicios.Rabbit.MensajeServiceUsersConfig;
@@ -31,14 +32,28 @@ public class MensajeControllerUsers {
 
     @PostMapping("/mensaje/enviar/usuarios")
     public String enviarMensajeUsuarios(@RequestBody Usuarios usuarios) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonUsuarios = objectMapper.writeValueAsString(usuarios);
-            this.template.convertAndSend(MensajeServiceUsersConfig.EXCHANGE_NAME, MensajeServiceUsersConfig.ROUTING_KEY_USERS, jsonUsuarios);
-            return "Usuarios publicados: " + usuarios.getNombre() + " con id: " + usuarios.getId();
-        } catch (JsonProcessingException e) {
-            return "Error al serializar la preferencia: " + e.getMessage();
-        }
+         try {
+        UsuariosDTO dto = new UsuariosDTO(
+
+            usuarios.getCorreo(),
+            usuarios.getContraseña(),
+            usuarios.getId(),
+            usuarios.getNombre(),
+            usuarios.getRol().getNombre() // Solo el nombre del rol, no el objeto completo
+
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonUsuarios = objectMapper.writeValueAsString(dto);
+        this.template.convertAndSend(
+            MensajeServiceUsersConfig.EXCHANGE_NAME,
+            MensajeServiceUsersConfig.ROUTING_KEY_USERS,
+            jsonUsuarios
+        );
+        return "Usuarios publicados: " + dto.getNombre() + " con id: " + dto.getId();
+    } catch (JsonProcessingException e) {
+        return "Error al serializar el usuario: " + e.getMessage();
+    }
     }
 
 
